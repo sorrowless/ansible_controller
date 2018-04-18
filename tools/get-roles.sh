e499f14b5cdbd487a2e67044802f7530271962a1
Stanislaw Bogatkin <sbog@sbog.ru>
2018-04-18 14:27:16 +0300

    Reinvent roles lists structure
    In case we checking roles each time (like in CI/CD pipelines), split
    them in parts to be able to checkout only needed pieces.

--- tools/get-roles.sh
+++ tools/get-roles.sh
@@ -1,4 +1,23 @@
 #!/bin/sh
 
-ansible-galaxy install -r get-non-vendor-roles.yml -p ./roles/
-ansible-galaxy install -r get-vendor-roles.yml -p ./roles/
+function get_roles() {
+  ROLE_PATH="tools/roles_lists/$1"
+  echo "Reading $ROLE_PATH list"
+  if ! grep -q src $ROLE_PATH; then
+    echo "Roles list $1 does not contain valid sources, skipping"
+    return 0
+  fi
+  ansible-galaxy install -r $ROLE_PATH -p ./roles/
+}
+
+if [ -z $1 ]; then
+  echo "No roles arguments was passed, checkout all roles"
+  for i in `ls tools/roles_lists/` ; do
+    get_roles $i
+  done
+else
+  for i in "$@" ; do
+    get_roles $i
+  done
+fi
+exit 0
