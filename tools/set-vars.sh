dedd34aa71988b455d64d039ebd317cf8deae345
Stan Bogatkin <stabog.tmb@gmail.com>
2019-10-22 19:59:06 +0300

    Add new script to set vars more fast
    New script which helps to set env variables to run ansible smoothier.
    Also update mitogen playbook.

---
+++ tools/set-vars.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# This small script set your env vars to automate consecutive Ansible runs.
+# It sets become pass, vault file, mitogen path. Beware that it uses env vars
+# and plain textfiles in /tmp/, so do not use it if you have insecure
+# environment then you should not use it as env vars can be read by another
+# user in some circumstances.
+#
+# How to use it: go to to controller repository source and run
+# > . ./tools/set-vars.sh
+#
+# then fill asked fields and that's it.
+#
+
+echo -n "Become password to export: "
+read -s ANSIBLE_BECOME_PASS
+export ANSIBLE_BECOME_PASS
+echo
+
+echo -n "Filename for vault: "
+read VAULT_FILENAME
+
+echo -n "Vault password: "
+read -s ANSIBLE_VAULT_REAL_PASS
+echo
+echo ${ANSIBLE_VAULT_REAL_PASS} > /tmp/${VAULT_FILENAME}
+export ANSIBLE_VAULT_PASSWORD_FILE=/tmp/${VAULT_FILENAME}
+
+echo "Check for mitogen installation"
+stat library 2>&1 1>/dev/null
+MITOGEN_PATH=$(find library -maxdepth 1 -type d -iname 'mitogen*')
+if [[ -n "${MITOGEN_PATH}" ]]; then
+  echo "Mitogen found in ${MITOGEN_PATH}, enable it"
+  export ANSIBLE_STRATEGY_PLUGINS="${MITOGEN_PATH}/ansible_mitogen/plugins/strategy"
+  export ANSIBLE_STRATEGY=mitogen_linear
+  echo "Mitogen variables exported successfully"
+else
+  echo "Looks that Mitogen is not installed to local library, skip initialize"
+fi
+
+echo "You're all set, bye"
