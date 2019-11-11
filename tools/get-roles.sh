0fcbe986a52890c5a002eae7c9dc425aa8b8a737
Stan Bogatkin <stabog.tmb@gmail.com>
2019-11-11 16:39:00 +0300

    Add pyenv playbook
    Add pyenv role to roles lists and playbook to install pyenv to target
    node. Also create example variables in group vars.
    
    Update the script to getting roles from lists. Now one can just call
    
    > ./tools/get-roles <part of filename with roles>
    
    e.g.:
    
    > ./tools/get-roles python
    
    which will find `python-roles.yml` automatically and install needed
    roles from it.

--- tools/get-roles.sh
+++ tools/get-roles.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 
+ROLES_PATH="tools/roles_lists"
+
 function get_roles {
-  ROLE_PATH="tools/roles_lists/$1"
+  stat "$ROLES_PATH/$1" >/dev/null 2>&1
+  IS_FILENAME=$?
+  if [[ "$IS_FILENAME" -ne 0 ]]; then
+    echo "$1 does not look like filename, trying to find according roles"
+    pushd "$ROLES_PATH" || exit 1
+    for i in *.yml; do
+      ROLE_NAME=$(echo "$i" | grep -i "$1")
+      if [[ -n "$ROLE_NAME" ]]; then
+        ROLE_PATH="$ROLES_PATH/$ROLE_NAME"
+        echo "Found roles list for $1 - $ROLE_NAME, will get roles from it"
+        break
+      fi
+    done
+    popd || exit 1
+  else
+    ROLE_PATH="$ROLES_PATH/$1"
+  fi
+
   echo "Reading $ROLE_PATH list"
   if ! grep -q src "$ROLE_PATH"; then
     echo "Roles list $1 does not contain valid sources, skipping"
@@ -17,7 +36,7 @@ fi
 
 if [ -z "$1" ]; then
   echo "No roles arguments was passed, checkout all roles"
-  for i in tools/roles_lists/*.yml ; do
+  for i in "$ROLES_PATH"/*.yml ; do
     get_roles "$i"
   done
 else
