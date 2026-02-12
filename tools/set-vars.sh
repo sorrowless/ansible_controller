7d45fec1848aa3b4cbe081d10ad5c9149c319c3e
Stan Bogatkin <s@sbog.ru>
2026-02-12 14:19:22 +0300

    Add new hosts

--- tools/set-vars.sh
+++ tools/set-vars.sh
@@ -27,28 +27,32 @@ else
   rl="readlink"
 fi
 
-echo -n "Become password to export: "
-read -r -s ANSIBLE_BECOME_PASS
-export ANSIBLE_BECOME_PASS
-echo
-
 #echo -n "Filename for vault: "
 #read -r VAULT_FILENAME
-
-echo -n "Vault password: "
-read -r -s ANSIBLE_VAULT_REAL_PASS
-echo
-export ANSIBLE_VAULT_REAL_PASS
-if [[ -n "$BASH_SOURCE" ]]; then
-  SCRIPT=$($rl -f "$BASH_SOURCE")
+if (which rbw); then
+  echo 'setting vault pass by rbw'
+  export ANSIBLE_VAULT_REAL_PASS=$(rbw get 'akh vault password')
 else
-  SCRIPT=$($rl -f "$0")
+  echo -n "Become password to export: "
+  read -r -s ANSIBLE_BECOME_PASS
+  export ANSIBLE_BECOME_PASS
+  echo
+
+  echo -n "Vault password: "
+  read -r -s ANSIBLE_VAULT_REAL_PASS
+  echo
+  export ANSIBLE_VAULT_REAL_PASS
+  if [[ -n "$BASH_SOURCE" ]]; then
+    SCRIPT=$($rl -f "$BASH_SOURCE")
+  else
+    SCRIPT=$($rl -f "$0")
+  fi
+  SCRIPT_PATH=$(dirname "$SCRIPT")
+  VAULT_EXEC_FILE="$SCRIPT_PATH/get-vault-pass"
+  #echo "${ANSIBLE_VAULT_REAL_PASS}" > "/tmp/${VAULT_FILENAME}"
+  #export ANSIBLE_VAULT_PASSWORD_FILE=/tmp/${VAULT_FILENAME}
+  export ANSIBLE_VAULT_PASSWORD_FILE="$VAULT_EXEC_FILE"
 fi
-SCRIPT_PATH=$(dirname "$SCRIPT")
-VAULT_EXEC_FILE="$SCRIPT_PATH/get-vault-pass"
-#echo "${ANSIBLE_VAULT_REAL_PASS}" > "/tmp/${VAULT_FILENAME}"
-#export ANSIBLE_VAULT_PASSWORD_FILE=/tmp/${VAULT_FILENAME}
-export ANSIBLE_VAULT_PASSWORD_FILE="$VAULT_EXEC_FILE"
 
 echo "Check for mitogen installation"
 stat library >/dev/null 2>&1
